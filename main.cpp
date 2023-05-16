d8d9e7b33ee0a7df2bb8f9d4ca27162127c45632
ingT6481221 <132637600+ingT6481221@users.noreply.github.com>
2023-05-16 09:07:55 +0000

    finish

--- main.cpp
+++ main.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
+#include <string.h>
+#include <iomanip>
 using namespace std;
 
 int main(){
-
-  int a;
+  /*int a;
   double d;
   cout<<&a<<" size "<<sizeof(a) <<endl;
   cout<<&d<<" size "<<sizeof(d) <<endl; 
   
-  /*  int x,*p;
+  int x,*p;
     p=&x;
     x=5;
 
@@ -17,8 +18,8 @@ int main(){
     cout<< &p <<endl;*/
 
 /*Example Array*/
-  /*
-int *ip;
+  
+/*int *ip;
 int a[ ] = {31415, 21828};
 ip = a;
 // alternative:	ip = &a[0];
@@ -33,8 +34,37 @@ cout<<ip<<" "<<*ip<<endl;
 ip++; 
 cout<<a<<endl;		
 cout<<ip<<" "<<*ip<<endl; 	// print 
-*/
- /*the address is 4324, not 4321
+
+ the address is 4324, not 4321
  because the pointer is incremented 						
- by the size of an integer (4 bytes) */
+ by the size of an integer (4 bytes) 8*/
+  char *pa, *pb, i ,temp;
+  char a[1000];
+  //pa = &a; //pa =a;
+  cout<<"Input: ";
+  cin>>a;
+  int SIZE = strlen(a);
+
+  pa = &a[0];
+  cout<<"Original:";
+  for (i=0; i<SIZE-1; i++ ,pa++)
+    {
+      cout<<setw(3)<<*pa;
+    }
+  cout<<setw(3)<<*pa<<endl;
+  pa = &a[0]; pb = &a[SIZE -1];
+  cout<<"Reverse: ";
+  for(i=0; i<SIZE/2;i++)
+    {
+      temp = *pa;
+      *pa = *pb;
+      *pb = temp;
+      pa++; pb--;
+    }
+  pa = &a[0];
+  for(i=0;i<SIZE-1;i++,pa++)
+    cout<<setw(3)<<*pa;
+
+  cout<<setw(3)<<*pa<<endl;
+  return 0;
 }
